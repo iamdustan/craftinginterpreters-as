90133f5f037a95af280975b3d81210601ae8c1bf
Dustan Kasten <dustan.kasten@gmail.com>
2022-02-11 09:28:41 -0500

    initial commit. mostly through chapter 4

---
+++ tests/index.js
@@ -0,0 +1,25 @@
+const test = require('ava');
+const m = require('..');
+
+test('tokenizer: comments are dropped', async (t) => {
+  const input = '// this is a comment';
+  t.deepEqual(await m.__tokenize(input), ['EOF']);
+});
+
+test('tokenizer: can do grouping things', async (t) => {
+  const input = '(( )){}';
+  t.deepEqual(await m.__tokenize(input), [
+    'LEFT_PAREN',
+    'LEFT_PAREN',
+    'RIGHT_PAREN',
+    'RIGHT_PAREN',
+    'LEFT_BRACE',
+    'RIGHT_BRACE',
+    'EOF',
+  ]);
+});
+
+test('tokenizer', async (t) => {
+  const input = `var language = "lox";`;
+  t.deepEqual(await m.__tokenize(input), ['VAR', 'language', '=', '"lox"']);
+});
