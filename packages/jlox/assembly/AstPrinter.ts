2d03b2c35056b63becf5cfb2fa1e563c7cd17e7f
Dustan Kasten <dustan.kasten@gmail.com>
2022-02-15 23:27:49 -0500

    maybe test out the printer

---
+++ packages/jlox/assembly/AstPrinter.ts
@@ -0,0 +1,24 @@
+import { Expr, Binary, Grouping, Literal, Unary, Visitor } from './Expr';
+
+interface Visitor<T> {
+  accept(visitor: Expr): T;
+}
+
+class AstPrinter implements Visitor<string> {
+  print(expr: Expr): string {
+    return expr.accept(this);
+  }
+  visitBinaryExpr(expr: Expr.Binary): string {
+    return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+  }
+  visitGroupingExpr(expr: Expr.Grouping): string {
+    return parenthesize('group', expr.expression);
+  }
+  visitLiteralExpr(expr: Expr.Literal): string {
+    if (expr.value == null) return 'nil';
+    return expr.valuie.toString();
+  }
+  visitUnaryExpr(expr: Expr.Unary): string {
+    return parenthesize(expr.operator.lexeme, expr.right);
+  }
+}
